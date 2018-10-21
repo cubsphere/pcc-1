09a0570391e823b209a84c8e61a3f2fd2a9f1457
Pedro Queiroga <psq@cin.ufpe.br>
2018-10-21 06:52:42 -0300

    helpful string agora diz que tem ukkers e sellonen

--- src/include/helpful-string.hpp
+++ src/include/helpful-string.hpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 char const* helpful_string =
-"pmt [options] pattern textfile [textfile...]\noptions:\n-e, --edit i: set maximum editing distance to i.\n-p, --pattern patternfile: search for all patterns in patternfile. patterns must be separated by a line break. if this option is used, then arguments are instead read as:\n    pmt [options] textfile [textfile...]\n-a, --algorithm algorithm_name: uses the algorithm algorithm_name to perform the search. algorithm_name may be any one of:\n    boyer-moore\n    shift-or\n-c, --count: display only the amount of occurences of each pattern in each text.\n-h, --help: display this message.\n";
+"pmt [options] pattern textfile [textfile...]\noptions:\n-e, --edit i: set maximum editing distance to i.\n-p, --pattern patternfile: search for all patterns in patternfile. patterns must be separated by a line break. if this option is used, then arguments are instead read as:\n    pmt [options] textfile [textfile...]\n-a, --algorithm algorithm_name: uses the algorithm algorithm_name to perform the search. algorithm_name may be any one of:\n    boyer-moore\n    shift-or\n    sellers\n    ukkonen\n-c, --count: display only the amount of occurences of each pattern in each text.\n-h, --help: display this message.\n";
 
 #endif
